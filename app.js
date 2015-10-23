0462a09aba6ad2ac4de19901616eff7962e3b59c
John Culviner <john.culviner@gmail.com>
2015-10-23 00:54:43 -0500

    2-lumber-jack

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-var app = require('express')();
-var childProcess = require('child_process');
-var fs = require('fs');
+var Promise = require('bluebird');
+var app = Promise.promisifyAll(require('express')());
+var childProcess = Promise.promisifyAll(require('child_process'));
+var fs = Promise.promisifyAll(require('fs'));
 
 app.get('/ping', (req, res) => {
-  fs.readFile(__filename, (err, file) => {
-    childProcess.exec('top -stats command,cpu -l 1', (error, stdout) => {
+
+  Promise.join(
+    fs.readFileAsync(__filename),
+    childProcess.execAsync('top -stats command,cpu -l 1')
+  )
+    .spread((file, stdout) => {
       res.json({
         currentFile: file.toString().split('\n'),
-        topProcesses: stdout.split('\n')
+        topProcesses: stdout[0].split('\n')
       });
     });
-  });
 });
 
-app.listen(3000, () => console.log('server up!'));
+app.listenAsync(3000)
+  .then(() => console.log('server up!'));
 
